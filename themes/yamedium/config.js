f92ba7e8b58757e24a8a46e0af36a2a4ab2b7793
hanff <hanffeix@outlook.com>
2024-09-21 22:39:39 +0800

    yamedium theme config change

--- themes/yamedium/config.js
+++ themes/yamedium/config.js
@@ -4,14 +4,14 @@ const CONFIG = {
   MEDIUM_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || false, // 右侧面板深色模式
 
   MEDIUM_POST_LIST_COVER: true, // 文章列表显示图片封面
-  MEDIUM_POST_LIST_PREVIEW: true, // 列表显示文章预览
+  MEDIUM_POST_LIST_PREVIEW: false, // 列表显示文章预览
   MEDIUM_POST_LIST_CATEGORY: true, // 列表显示文章分类
   MEDIUM_POST_LIST_TAG: true, // 列表显示文章标签
 
   MEDIUM_POST_DETAIL_CATEGORY: true, // 文章显示分类
   MEDIUM_POST_DETAIL_TAG: true, // 文章显示标签
 
-  MEDIUM_SHOW_INFO_CARDS: false, //文章卡片
+  MEDIUM_SHOW_INFO_CARDS: true, //个人简介卡片
 
   // 菜单
   MEDIUM_MENU_CATEGORY: true, // 显示分类
